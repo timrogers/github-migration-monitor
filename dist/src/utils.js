009f3d34883f41a39feec7df43fd49cea3c471df
Tim Rogers <timrogers@github.com>
2023-04-21 18:39:17 +0100

    chore: remove dist directory from Git

--- dist/src/utils.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.presentState = void 0;
-const presentState = (state) => state.replace('_', ' ').toLowerCase();
-exports.presentState = presentState;
