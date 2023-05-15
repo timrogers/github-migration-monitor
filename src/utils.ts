4bcebad6039f0bc1712c878e6a15cfe7186473bd
Tim Rogers <timrogers@github.com>
2023-05-15 12:17:16 +0100

    Improve the script's resilience when the API returns an error
    This improves the resilient of the script errors returned by
    the GitHub API, aiming to log and retry rather than explode the
    script.
    
    Fixes #10 and #11.

--- src/utils.ts
+++ src/utils.ts
@@ -1,1 +1,11 @@
 export const presentState = (state: string): string => state.replace('_', ' ').toLowerCase();
+
+export const serializeError = (error: any): string => {
+    if (typeof error === 'string') {
+        return error;
+    } else {
+        return JSON.stringify(error);
+    }
+};
+
+export const sleep = (ms: number) => new Promise((r) => setTimeout(r, ms));
